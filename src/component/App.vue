d0e62d009a5a41d6201cccba27f5465d68d10574
liaohuida <email529800@163.com>
2018-03-18 01:06:05 +0800

    脚手架

---
+++ src/component/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        height: 100%;
+        background-color: rgb(38, 124, 183);
+    }
+</style>
